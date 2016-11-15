564ccc1688df16a5b2bac487d9dc185e31994e16
Dario Caruso <darioyzf@gmail.com>
2016-11-15 17:16:28 +0100

    start

---
+++ index.ts
@@ -0,0 +1,41 @@
+
+import * as _ from 'lodash'
+
+interface Ibuildconf {
+
+}
+
+export default class Place {
+
+constructor(buildingsconf:Ibuildconf) {
+
+}
+
+getBuildings(){
+
+}
+
+getBuilding(){
+    
+}
+
+getFloors(){
+    
+}
+getFloor(){
+    
+}
+
+
+getRooms(){
+    
+}
+getRoom(){
+    
+}
+addObject(){
+
+}
+
+
+}
